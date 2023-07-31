3e4c6fdbc9367cec3559872896ba1c1565022c90
21ulas21 <ulasegrk21@hotmail.com>
2023-07-31 18:08:25 +0300

    a

--- src/main/java/com/egrikulas/pinsoft/PinsoftApplication.java
+++ src/main/java/com/egrikulas/pinsoft/PinsoftApplication.java
@@ -2,8 +2,10 @@ package com.egrikulas.pinsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PinsoftApplication {
 
 	public static void main(String[] args) {
